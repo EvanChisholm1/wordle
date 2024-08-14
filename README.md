1f739b4696c887c36f87e845af51fd9d6d897c72
Evan Chisholm <elchisholm248@gmail.com>
2024-08-13 21:41:23 -0400

    finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# wordle
+
+Speedrunning writing wordle, final time 20 minutes and 13 seconds, also kinda code golfed it a bit.
---
+++ index.ts
@@ -0,0 +1,43 @@
+const words = (await Bun.file("words.txt").text()).split("\n");
+const hiddenWord = words[Math.floor(Math.random() * words.length)];
+const correctChars = hiddenWord.split("");
+const tries: string[] = [];
+
+const keepGoing = () =>
+    tries.length >= 5 || tries.at(-1) === hiddenWord ? false : true;
+
+const red = (text: string) => `\x1b[31m${text}\x1b[0m`;
+const yellow = (text: string) => `\x1b[33m${text}\x1b[0m`;
+const green = (text: string) => `\x1b[32m${text}\x1b[0m`;
+
+function printValidity(guess: string) {
+    return guess
+        .split("")
+        .map((x, i) =>
+            x === correctChars[i]
+                ? green(x)
+                : correctChars.includes(x)
+                ? yellow(x)
+                : x
+        )
+        .join(" ");
+}
+
+while (keepGoing()) {
+    console.log("guesses left:", 5 - tries.length);
+    const guess = prompt("enter a five letter word you would like to guess:")!;
+
+    if (guess.length !== 5 || !words.includes(guess)) {
+        console.log("you did not input a valid word try again");
+        continue;
+    }
+
+    tries.push(guess);
+    console.log(printValidity(guess));
+}
+
+if (tries.at(-1) === hiddenWord) {
+    console.log("congrats you win, the correct word is", green(hiddenWord));
+} else {
+    console.log("sorry you lost the correct word is", red(hiddenWord));
+}
